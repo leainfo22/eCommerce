a5dc03ac25c43588cd52e9582765e723021e13bf
leainfo22 <leandro.vasquez.mora@gmail.com>
2025-03-17 17:35:50 +1100

    Draft Infrastructure.

---
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -0,0 +1,22 @@
+using eCommerce.Infrastructure;
+using eCommerce.Core;
+var builder = WebApplication.CreateBuilder(args);
+
+
+//Add infrastructure services
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+builder.Services.AddControllers();
+
+//Build the web app
+var app = builder.Build();
+
+//Routing
+app.UseRouting();
+
+app.UseAuthentication();    
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
---
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace eCommerce.Infrastructure;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    ///  Extension method to add infrastructure services to the dependency  injection container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    
+    }
+}
