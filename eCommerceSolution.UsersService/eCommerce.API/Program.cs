a53a316ae0555a7105b78f5c1574f29457b5e03e
leainfo22 <leandro.vasquez.mora@gmail.com>
2025-04-07 20:55:13 +1000

    Swagger added

--- eCommerceSolution.UsersService/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -24,11 +24,30 @@ builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
 //Add FluentValidation
 builder.Services.AddFluentValidationAutoValidation();
 
+//Add API explorer services
+builder.Services.AddEndpointsApiExplorer();
+//Add Swagger services
+builder.Services.AddSwaggerGen();
+//Add cors services
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 //Build the web app
 var app = builder.Build();
 app.UseExceptionHandlingMiddleware();
 //Routing
 app.UseRouting();
+//using swagger
+app.UseSwagger();
+//add swageger UI
+app.UseSwaggerUI();
 
 app.UseAuthentication();    
 app.UseAuthorization();
